a8af29081ce75e339d6a1f85fd05960e14e6f277
sp8783 <p.sp8783@gmail.com>
2023-06-12 10:46:37 +0900

    use Date object's Range instead of Integer object's Range for the date loop in 'print_calendar' method

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -17,16 +17,16 @@ end
 # 指定した年月のカレンダーを表示する
 def print_calendar(year, month)
   first_date = Date.new(year, month, 1)
-  last_day = Date.new(year, month, -1).day
+  last_date = Date.new(year, month, -1)
 
   puts first_date.strftime("%B %Y").center(20)
   puts "Su Mo Tu We Th Fr Sa"
 
   print_line = " " * 3 * first_date.wday
-  
-  (1..last_day).each do |day|
-    print_line += day.to_s.rjust(2) + " "
-    if print_line.length == 3 * 7 or day == last_day
+
+  (first_date..last_date).each do |date|
+    print_line += date.day.to_s.rjust(2) + " "
+    if date.saturday? or date == last_date
       puts print_line
       print_line = ""
     end

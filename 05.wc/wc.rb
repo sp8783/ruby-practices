093610524093d932b9c22d9de4afe63d517af93c
sp8783 <p.sp8783@gmail.com>
2023-07-25 21:08:43 +0900

    rename some methods and variables

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -21,27 +21,27 @@ end
 
 def wc_with_no_argument(display_flags)
   text = $stdin.read
-  file_details_list = [make_file_details(text, '', 'stdin')]
-  display_file_details(file_details_list, display_flags)
+  file_details = [make_file_detail(text, '', 'stdin')]
+  display_file_details(file_details, display_flags)
 end
 
 def wc_with_filepaths(filepaths, display_flags)
-  file_details_list = []
+  file_details = []
   filepaths.each do |filepath|
     if File.file?(filepath)
       text = File.read(filepath)
-      file_details_list << make_file_details(text, filepath, 'file')
+      file_details << make_file_detail(text, filepath, 'file')
     elsif File.directory?(filepath)
-      file_details_list << make_file_details('', filepath, 'directory')
+      file_details << make_file_detail('', filepath, 'directory')
     else
-      file_details_list << make_file_details('', filepath, 'no_exist')
+      file_details << make_file_detail('', filepath, 'no_exist')
     end
   end
-  file_details_list << make_total_value_to_file_details(file_details_list) if file_details_list.size >= 2
-  display_file_details(file_details_list, display_flags)
+  file_details << make_total_value_to_file_detail(file_details) if file_details.size >= 2
+  display_file_details(file_details, display_flags)
 end
 
-def make_file_details(text, filepath, category)
+def make_file_detail(text, filepath, category)
   {
     line: count_lines(text),
     word: count_words(text),
@@ -63,18 +63,18 @@ def count_bytes(text)
   text.bytesize
 end
 
-def make_total_value_to_file_details(file_details_list)
+def make_total_value_to_file_detail(file_details)
   {
-    line: file_details_list.sum { |hash| hash[:line] },
-    word: file_details_list.sum { |hash| hash[:word] },
-    byte: file_details_list.sum { |hash| hash[:byte] },
+    line: file_details.sum { |hash| hash[:line] },
+    word: file_details.sum { |hash| hash[:word] },
+    byte: file_details.sum { |hash| hash[:byte] },
     name: 'total'
   }
 end
 
-def display_file_details(file_details_list, display_flags)
-  column_width = calc_column_width(file_details_list, display_flags)
-  file_details_list.each do |hash|
+def display_file_details(file_details, display_flags)
+  column_width = calc_column_width(file_details, display_flags)
+  file_details.each do |hash|
     if hash[:category] == 'no_exist'
       puts "wc: #{hash[:name]}: No such file or directory"
       next
@@ -89,14 +89,14 @@ def display_file_details(file_details_list, display_flags)
   end
 end
 
-def calc_column_width(file_details_list, display_flags)
-  if display_flags.values.count(true) == 1 && file_details_list.size == 1
+def calc_column_width(file_details, display_flags)
+  if display_flags.values.count(true) == 1 && file_details.size == 1
     return 0 # 「（ファイル名を除く）表示列が1列」かつ「入力が1ファイルのみ」の場合、本物のwcコマンドでは空白埋めをしない
   end
 
-  target_file_details = file_details_list[-1] # 入力が1ファイルの場合はそのファイルの行を、複数ファイルの場合はtotal行を元に列幅を計算する（total行が一番文字数が多いため）
+  target_file_details = file_details[-1] # 入力が1ファイルの場合はそのファイルの行を、複数ファイルの場合はtotal行を元に列幅を計算する（total行が一番文字数が多いため）
   maximum_value_length = target_file_details.values_at(*%i[line word byte]).map { |value| value.to_s.size }.max
-  if target_file_details[:category] == 'stdin' || file_details_list.map { |h| h[:category] }.include?('directory')
+  if target_file_details[:category] == 'stdin' || file_details.map { |h| h[:category] }.include?('directory')
     [maximum_value_length, 7].max # 「標準入力」または「入力にディレクトリを含む」の場合、本物のwcコマンドでは列幅が半角7文字分になる
   else
     maximum_value_length

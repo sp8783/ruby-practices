f55ec6e49629f522471765f9879d70a8ce2b198b
sp8783 <p.sp8783@gmail.com>
2024-03-20 08:18:04 +0900

    Change the function of `LongFormatter` and `ShortFormatter` classes to only retrieve metadata

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -3,6 +3,9 @@
 require_relative 'short_formatter'
 require_relative 'long_formatter'
 
+NUMBER_OF_COLUMNS = 3
+WIDTH_BETWEEN_COLUMNS = 2
+
 class LsCommand
   def initialize(path, options)
     @path = path || '.'
@@ -14,8 +17,12 @@ class LsCommand
     is_file = @path && FileTest.file?(@path)
 
     formatter = @options['l'] ? LongFormatter : ShortFormatter
-    formatter.new(all_file_paths, is_file).format.each do |line|
-      puts line
+    all_metadata = formatter.new(all_file_paths, is_file).format
+
+    if @options['l']
+      display_files_for_long_format(all_metadata)
+    else
+      display_files_for_short_format(all_metadata)
     end
   end
 
@@ -39,5 +46,52 @@ class LsCommand
   def sort_file_paths(all_file_paths)
     all_file_paths.sort! { |x, y| x.casecmp(y).nonzero? || y <=> x }
     @options['r'] ? all_file_paths.reverse : all_file_paths
+  end
+
+  def display_files_for_long_format(all_file_details)
+    if all_file_details[0].key?(:total_blocks)
+      total_blocks = all_file_details.shift[:total_blocks]
+      puts "total #{total_blocks}"
+    end
+    max_lengths = calc_max_length_for_variable_length_columns(all_file_details)
+    all_file_details.each do |detail|
+      detail.delete(:block)
+      puts detail.map { |k, v| v.rjust(max_lengths[k]) }.join(' ')
+    end
+  end
+
+  def calc_max_length_for_variable_length_columns(all_file_details)
+    variable_length_columns = %i[hardlink user_name group_name file_size]
+    max_lengths = Hash.new(0)
+    all_file_details.each do |detail|
+      variable_length_columns.each { |col| max_lengths[col] = [max_lengths[col], detail[col].size].max }
+    end
+    max_lengths
+  end
+
+  def display_files_for_short_format(all_files)
+    num_rows = (all_files.size.to_f / NUMBER_OF_COLUMNS).ceil
+    widths_per_column = calculation_width_columns(all_files, num_rows)
+    (0...num_rows).each do |row|
+      puts all_files.select.each_with_index { |_, i| i % num_rows == row }
+                    .map.with_index { |file, i| ljust_for_multibyte_characters(file, widths_per_column[i]) }
+                    .join
+    end
+  end
+
+  def calculation_width_columns(all_files, num_rows)
+    all_files.each_slice(num_rows).map do |files|
+      files.map { |file| size_for_multibyte_characters(file) }.max + WIDTH_BETWEEN_COLUMNS
+    end
+  end
+
+  def ljust_for_multibyte_characters(string, width, padding = ' ')
+    multibyte_width = size_for_multibyte_characters(string)
+    padding_size = [0, width - multibyte_width].max
+    string + padding * padding_size
+  end
+
+  def size_for_multibyte_characters(string)
+    string.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.sum
   end
 end
--- 07.ls_object/short_formatter.rb
+++ 07.ls_object/short_formatter.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-NUMBER_OF_COLUMNS = 3
-WIDTH_BETWEEN_COLUMNS = 2
-
 class ShortFormatter
   def initialize(all_file_paths, is_file)
     @all_file_paths = all_file_paths
@@ -12,40 +9,10 @@ class ShortFormatter
   # 画面にファイル一覧と各ファイルの詳細情報を出力用の形式にする（lオプションがない場合）
   def format
     # コマンドライン引数にファイルが与えられている場合は、ファイル名=ファイルパスにする必要がある
-    all_files =
-      if @is_file
-        @all_file_paths
-      else
-        @all_file_paths.map { |file_path| File.basename(file_path) }
-      end
-
-    num_rows = (all_files.size.to_f / NUMBER_OF_COLUMNS).ceil
-    widths_per_column = calculation_width_columns(all_files, num_rows)
-    (0...num_rows).map do |row|
-      all_files.select.each_with_index { |_, i| i % num_rows == row }
-               .map.with_index { |file, i| ljust_for_multibyte_characters(file, widths_per_column[i]) }
-               .join
+    if @is_file
+      @all_file_paths
+    else
+      @all_file_paths.map { |file_path| File.basename(file_path) }
     end
-  end
-
-  private
-
-  # 出力時の幅を列毎に計算する
-  def calculation_width_columns(all_files, num_rows)
-    all_files.each_slice(num_rows).map do |files|
-      files.map { |file| size_for_multibyte_characters(file) }.max + WIDTH_BETWEEN_COLUMNS
-    end
-  end
-
-  # マルチバイト文字を考慮して左詰めした文字列を返す
-  def ljust_for_multibyte_characters(string, width, padding = ' ')
-    multibyte_width = size_for_multibyte_characters(string)
-    padding_size = [0, width - multibyte_width].max
-    string + padding * padding_size
-  end
-
-  # 「マルチバイト文字1文字 = 半角文字2文字」として文字列の長さをカウントする
-  def size_for_multibyte_characters(string)
-    string.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.sum
   end
 end

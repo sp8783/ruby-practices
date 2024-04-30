7baefe16e68030f3f0c28efad040c6afe7107fc3
sp8783 <p.sp8783@gmail.com>
2024-04-30 13:28:06 +0900

    Change `all_file_details` to be instance variable in `LongFormatter` class

--- 07.ls_object/long_formatter.rb
+++ 07.ls_object/long_formatter.rb
@@ -5,18 +5,17 @@ require_relative 'file_detail'
 
 class LongFormatter
   def initialize(all_file_paths, is_file)
-    @all_file_paths = all_file_paths
+    @all_file_details = all_file_paths.map { |file_path| FileDetail.new(file_path, is_file) }
     @is_file = is_file
   end
 
   def format
-    all_file_details = make_all_file_details
-    max_lengths = calculation_max_length_for_variable_length_columns(all_file_details)
-    total_blocks = calculation_total_blocks(all_file_details)
+    max_lengths = calculation_max_length_for_variable_length_columns
+    total_blocks = calculation_total_blocks
 
     print_format = @is_file ? [] : [['total', total_blocks.to_s].join(' ')]
     print_cols = %i[permission hardlink user_name group_name file_size timestamp file_name]
-    all_file_details.each do |detail|
+    @all_file_details.each do |detail|
       print_format << print_cols.map { |col| detail.send(col).rjust(max_lengths[col]) }.join(' ')
     end
     print_format
@@ -24,21 +23,17 @@ class LongFormatter
 
   private
 
-  def make_all_file_details
-    @all_file_paths.map { |file_path| FileDetail.new(file_path, @is_file) }
-  end
-
-  def calculation_max_length_for_variable_length_columns(all_file_details)
+  def calculation_max_length_for_variable_length_columns
     variable_length_columns = %i[hardlink user_name group_name file_size]
     max_lengths = Hash.new(0)
-    all_file_details.each do |detail|
+    @all_file_details.each do |detail|
       variable_length_columns.each { |col| max_lengths[col] = [max_lengths[col], detail.send(col).size].max }
     end
     max_lengths
   end
 
-  def calculation_total_blocks(all_file_details)
-    all_file_details.map(&:block).sum / 2 # Linuxのブロック数 = File::Statのブロック数 / 2
+  def calculation_total_blocks
+    @all_file_details.map(&:block).sum / 2 # Linuxのブロック数 = File::Statのブロック数 / 2
   end
 
   def convert_stat_mode_to_permission_code_for_ls_command(stat)
